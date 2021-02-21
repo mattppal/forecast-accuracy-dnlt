00c336cc9bb12ee7e1716b1c9f74c0f22546f68b
matt <matt.palmer@storyblocks.com>
2021-02-21 11:26:52 -0500

    pipeline rename

--- forecastadvisor/pipelines.py
+++ forecastadvisor/pipelines.py
@@ -7,24 +7,29 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from scrapy.exporters import CsvItemExporter
+import os
 
 class ForecastadvisorPipeline:
     def open_spider(self, spider):
-        self.year_to_exporter = {}
+        self.state_to_exporter = {}
 
     def close_spider(self, spider):
-        for exporter in self.year_to_exporter.values():
+        for exporter in self.state_to_exporter.values():
             exporter.finish_exporting()
 
     def _exporter_for_item(self, item):
         adapter = ItemAdapter(item)
         sort_state = adapter['sort_state']
-        if sort_state not in self.year_to_exporter:
+        
+        if not os.path.exists('./payloads'):
+            os.makedirs('./payloads')
+
+        if sort_state not in self.state_to_exporter:
             f = open(f'./payloads/{sort_state}.csv', 'wb')
             exporter = CsvItemExporter(f)
             exporter.start_exporting()
-            self.year_to_exporter[sort_state] = exporter
-        return self.year_to_exporter[sort_state]
+            self.state_to_exporter[sort_state] = exporter
+        return self.state_to_exporter[sort_state]
 
     def process_item(self, item, spider):
         exporter = self._exporter_for_item(item)
